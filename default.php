cad9bd774e7562fdecf091cbfb192ed967237077
O&R Associados <orassociados@gmail.com>
2017-01-03 20:27:03 -0200

    Add files via upload

---
+++ default.php
@@ -0,0 +1,105 @@
+
+<!DOCTYPE html>
+<html  dir="ltr" lang="pt-br" xml:lang="pt-br">
+<head>
+    <title>Moodle Citibank: Acesso ao site</title>
+    <link rel="shortcut icon" href="http://www.academiadevendasciti.com.br/moodle/theme/image.php/citibank/theme/1394723079/favicon" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="moodle, Moodle Citibank: Acesso ao site" />
+<script type="text/javascript">
+//<![CDATA[
+var M = {}; M.yui = {};
+M.pageloadstarttime = new Date();
+M.cfg = {"wwwroot":"http:\/\/www.academiadevendasciti.com.br\/moodle","sesskey":"6f1qJiW3rP","loadingicon":"http:\/\/www.academiadevendasciti.com.br\/moodle\/theme\/image.php\/citibank\/core\/1394723079\/i\/loading_small","themerev":"1394723079","slasharguments":1,"theme":"citibank","jsrev":"1390942427","svgicons":true,"developerdebug":true};var yui1ConfigFn = function(me) {if(/-skin|reset|fonts|grids|base/.test(me.name)){me.type='css';me.path=me.path.replace(/\.js/,'.css');me.path=me.path.replace(/\/yui2-skin/,'/assets/skins/sam/yui2-skin')}};
+var yui2ConfigFn = function(me) {var parts=me.name.replace(/^moodle-/,'').split('-'),component=parts.shift(),module=parts[0],min='-min';if(/-(skin|core)$/.test(me.name)){parts.pop();me.type='css';min=''};if(module){var filename=parts.join('-');me.path=component+'/'+module+'/'+filename+min+'.'+me.type}else me.path=component+'/'+component+'.'+me.type};
+YUI_config = {"debug":true,"base":"http:\/\/www.academiadevendasciti.com.br\/moodle\/lib\/yuilib\/3.13.0\/","comboBase":"http:\/\/www.academiadevendasciti.com.br\/moodle\/theme\/yui_combo.php?","combine":true,"filter":"RAW","insertBefore":"firstthemesheet","groups":{"yui2":{"base":"http:\/\/www.academiadevendasciti.com.br\/moodle\/lib\/yuilib\/2in3\/2.9.0\/build\/","comboBase":"http:\/\/www.academiadevendasciti.com.br\/moodle\/theme\/yui_combo.php?","combine":true,"ext":false,"root":"2in3\/2.9.0\/build\/","patterns":{"yui2-":{"group":"yui2","configFn":yui1ConfigFn}}},"moodle":{"name":"moodle","base":"http:\/\/www.academiadevendasciti.com.br\/moodle\/theme\/yui_combo.php?m\/1390942427\/","combine":true,"comboBase":"http:\/\/www.academiadevendasciti.com.br\/moodle\/theme\/yui_combo.php?","ext":false,"root":"m\/1390942427\/","patterns":{"moodle-":{"group":"moodle","configFn":yui2ConfigFn}},"filter":"DEBUG","modules":{"moodle-core-notification":{"requires":["moodle-core-notification-dialogue","moodle-core-notification-alert","moodle-core-notification-confirm","moodle-core-notification-exception","moodle-core-notification-ajaxexception"]},"moodle-core-notification-dialogue":{"requires":["base","node","panel","event-key","dd-plugin"]},"moodle-core-notification-alert":{"requires":["moodle-core-notification-dialogue"]},"moodle-core-notification-confirm":{"requires":["moodle-core-notification-dialogue"]},"moodle-core-notification-exception":{"requires":["moodle-core-notification-dialogue"]},"moodle-core-notification-ajaxexception":{"requires":["moodle-core-notification-dialogue"]},"moodle-core-tooltip":{"requires":["base","node","io-base","moodle-core-notification","json-parse","widget-position","widget-position-align","event-outside","cache-base"]},"moodle-core-popuphelp":{"requires":["moodle-core-tooltip"]},"moodle-core-actionmenu":{"requires":["base","event","node-event-simulate"]},"moodle-core-chooserdialogue":{"requires":["base","panel","moodle-core-notification"]},"moodle-core-blocks":{"requires":["base","node","io","dom","dd","dd-scroll","moodle-core-dragdrop","moodle-core-notification"]},"moodle-core-formautosubmit":{"requires":["base","event-key"]},"moodle-core-dock":{"requires":["base","node","event-custom","event-mouseenter","event-resize","escape","moodle-core-dock-loader"]},"moodle-core-dock-loader":{"requires":["escape"]},"moodle-core-formchangechecker":{"requires":["base","event-focus"]},"moodle-calendar-eventmanager":{"requires":["base","node","event-mouseenter","overlay","moodle-calendar-eventmanager-skin"]},"moodle-course-management":{"requires":["base","node","io-base","moodle-core-notification-exception","json-parse","dd-constrain","dd-proxy","dd-drop","dd-delegate","node-event-delegate"]},"moodle-course-util":{"requires":["node"],"use":["moodle-course-util-base"],"submodules":{"moodle-course-util-base":{},"moodle-course-util-section":{"requires":["node","moodle-course-util-base"]},"moodle-course-util-cm":{"requires":["node","moodle-course-util-base"]}}},"moodle-course-categoryexpander":{"requires":["node","event-key"]},"moodle-mod_quiz-autosave":{"requires":["base","node","event","event-valuechange","node-event-delegate","io-form"]},"moodle-block_navigation-navigation":{"requires":["base","io-base","node","event-synthetic","event-delegate","json-parse"]},"moodle-filter_glossary-autolinker":{"requires":["base","node","io-base","json-parse","event-delegate","overlay","moodle-core-notification-alert"]},"moodle-tool_capability-search":{"requires":["base","node"]},"moodle-theme_bootstrapbase-bootstrap":{"requires":["node","selector-css3"]},"moodle-assignfeedback_editpdf-editor":{"requires":["base","event","node","io","graphics","json","event-move","querystring-stringify-simple","moodle-core-notification-dialog","moodle-core-notification-exception","moodle-core-notification-ajaxexception"]}}}},"modules":{"core_filepicker":{"name":"core_filepicker","fullpath":"http:\/\/www.academiadevendasciti.com.br\/moodle\/lib\/javascript.php\/1390942427\/repository\/filepicker.js","requires":["base","node","node-event-simulate","json","async-queue","io-base","io-upload-iframe","io-form","yui2-treeview","panel","cookie","datatable","datatable-sort","resize-plugin","dd-plugin","escape","moodle-core_filepicker"]}}};
+M.yui.loader = {modules: {}};
+
+//]]>
+</script>
+<link rel="stylesheet" type="text/css" href="http://www.academiadevendasciti.com.br/moodle/theme/yui_combo.php?rollup/3.13.0/yui-moodlesimple.css" /><script type="text/javascript" src="http://www.academiadevendasciti.com.br/moodle/theme/yui_combo.php?rollup/3.13.0/yui-moodlesimple.js"></script><script type="text/javascript" src="http://www.academiadevendasciti.com.br/moodle/theme/yui_combo.php?rollup/1390942427/mcore-debug.js"></script><script id="firstthemesheet" type="text/css">/** Required in order to fix style inclusion problems in IE with YUI **/</script><link rel="stylesheet" type="text/css" href="http://www.academiadevendasciti.com.br/moodle/theme/styles.php/citibank/1394723079/all" />
+<script type="text/javascript" src="http://www.academiadevendasciti.com.br/moodle/lib/javascript.php/1390942427/lib/javascript-static.js"></script>
+
+<meta name="robots" content="noindex" />    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body  id="page-login-index" class="format-site  path-login safari dir-ltr lang-pt_br yui-skin-sam yui3-skin-sam 107-20-205-53--moodle pagelayout-login course-1 context-1 notloggedin layout-option-langmenu">
+	
+	<div class="skiplinks"><a class="skip" href="#maincontent">Ir para o conteúdo principal</a></div>
+<script type="text/javascript">
+//<![CDATA[
+document.body.className += ' jsenabled';
+//]]>
+</script>
+
+
+    <div id="page-content" class="row-fluid">
+        <section id="region-main" class="span12">
+            <div role="main"><span id="maincontent"></span><div class="loginbox clearfix onecolumn">
+  <div class="loginpanel">
+    <h2>Acesso</h2>
+      <div class="subcontent loginsub">
+                <form action="http://www.academiadevendasciti.com.br/moodle/login/index.php" method="post" id="login"  >
+          <div class="loginform">
+            <div class="form-label"><label for="username">Nome de usuário</label></div>
+            <div class="form-input">
+              <input type="text" name="username" id="username" size="15" value="admin" />
+            </div>
+            <div class="clearer"><!-- --></div>
+            <div class="form-label"><label for="password">Senha</label></div>
+            <div class="form-input">
+              <input type="password" name="password" id="password" size="15" value=""  />
+              <input type="submit" id="loginbtn" value="Acesso" />
+            </div>
+          </div>
+            <div class="clearer"><!-- --></div>
+                            <div class="rememberpass">
+                  <input type="checkbox" name="rememberusername" id="rememberusername" value="1" checked="checked" />
+                  <label for="rememberusername">Lembrar usuário</label>
+              </div>
+                        <div class="clearer"><!-- --></div>
+          <div class="forgetpass"><a href="forgot_password.php">Esqueceu o seu usuário ou senha?</a></div>
+        </form>
+        <div class="desc">
+            O uso de Cookies deve ser permitido no seu navegador<span class="helptooltip"><a href="http://www.academiadevendasciti.com.br/moodle/help.php?component=moodle&amp;identifier=cookiesenabled&amp;lang=pt_br" title="Ajuda com O uso de Cookies deve ser permitido no seu navegador" aria-haspopup="true" target="_blank"><img src="http://www.academiadevendasciti.com.br/moodle/theme/image.php/citibank/core/1394723079/help" alt="Ajuda com O uso de Cookies deve ser permitido no seu navegador" class="iconhelp" /></a></span>        </div>
+      </div>
+
+      <div class="subcontent guestsub">
+        <div class="desc">
+          Alguns cursos podem permitir o acesso a visitantes        </div>
+        <form action="index.php" method="post" id="guestlogin">
+          <div class="guestform">
+            <input type="hidden" name="username" value="guest" />
+            <input type="hidden" name="password" value="guest" />
+            <input type="submit" value="Acessar como visitante" />
+          </div>
+        </form>
+      </div>
+     </div>
+</div>
+</div>        </section>
+    </div>
+	
+    <script type="text/javascript" src="http://www.academiadevendasciti.com.br/moodle/theme/javascript.php/citibank/1394723079/footer"></script>
+<script type="text/javascript">
+//<![CDATA[
+M.str = {"moodle":{"lastmodified":"\u00daltima atualiza\u00e7\u00e3o","name":"Nome","error":"Erro","info":"Informa\u00e7\u00e3o","morehelp":"Mais ajuda","loadinghelp":"Carregando...","cancel":"Cancelar","yes":"Sim"},"repository":{"type":"Tipo","size":"Tamanho","invalidjson":"palavra JSON inv\u00e1lida","nofilesattached":"Nenhum arquivo anexado","filepicker":"File picker","logout":"Sair","nofilesavailable":"Nenhum arquivo dispon\u00edvel","norepositoriesavailable":"Desculpe, nenhum dos seus reposit\u00f3rios atuais pode retornar arquivos no formato solicitado.","fileexistsdialogheader":"Arquivo existe","fileexistsdialog_editor":"Um arquivo com este nome j\u00e1 foi anexado ao texto que voc\u00ea est\u00e1 editando","fileexistsdialog_filemanager":"Um arquivo com este nome j\u00e1 foi anexado","renameto":"Renomear para \"{$a}\"","referencesexist":"H\u00e1 {$a} arquivos pseud\u00f4nimos\/atalhos que usam este arquivo como fonte","select":"Selecione"},"admin":{"confirmation":"Confirma\u00e7\u00e3o"}};
+//]]>
+</script>
+<script type="text/javascript">
+//<![CDATA[
+YUI().use('node', function(Y) {
+M.util.load_flowplayer();
+setTimeout("fix_column_widths()", 20);
+M.util.help_popups.setup(Y);
+M.yui.galleryversion="2010.04.08-12-35";Y.use("moodle-core-popuphelp",function() {M.core.init_popuphelp();
+});
+ M.util.js_pending('random5321cf13926072'); Y.on('domready', function() { M.util.js_complete("init");  M.util.js_complete('random5321cf13926072'); });
+
+});
+//]]>
+</script>
+
+</div>
+</body>
+</html>
